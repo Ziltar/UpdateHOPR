8e64b71ed6954f0600edfae3b12839339fdc0892
Ziltar <118578006+Ziltar@users.noreply.github.com>
2023-03-07 16:47:52 +0100

    Update updateHopr.sh
    new start command

--- updateHopr.sh
+++ updateHopr.sh
@@ -20,4 +20,4 @@ echo "Started Admin UI..."
 echo "API-Token:"
 read apiToken
 echo "Starting Node..."
-docker run --pull always --restart on-failure -m 2g --log-driver json-file --log-opt max-size=100M --log-opt max-file=5 -ti -v $HOME/.hoprd-db-monte-rosa:/app/hoprd-db -p 9091:9091 -p 3001:3001 -e DEBUG="hopr*" gcr.io/hoprassociation/hoprd:1.92.9 --environment monte_rosa --init --api --identity /app/hoprd-db/.hopr-id-monte-rosa --data /app/hoprd-db --password 'open-sesame-iTwnsPNg0hpagP+o6T0KOwiH9RQ0' --apiHost "0.0.0.0" --apiToken $apiToken --healthCheck --healthCheckHost "0.0.0.0"
+docker run --pull always --restart on-failure -m 2g --log-driver json-file --log-opt max-size=100M --log-opt max-file=5 -ti -v $HOME/.hoprd-db-monte-rosa:/app/hoprd-db -p 9091:9091/tcp -p 9091:9091/udp -p 8080:8080 -p 3001:3001 -e DEBUG="hopr*" gcr.io/hoprassociation/hoprd:1.92.9 --environment monte_rosa --init --api --identity /app/hoprd-db/.hopr-id-monte-rosa --data /app/hoprd-db --password 'open-sesame-iTwnsPNg0hpagP+o6T0KOwiH9RQ0' --apiHost "0.0.0.0" --apiToken $apiToken --healthCheck --healthCheckHost "0.0.0.0"
